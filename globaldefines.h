2df425b4baeda849fdcbbb9ca99e29f383046ba2
Erik Herrmann <erik.herrmann@informatik.tu-chemnitz.de>
2014-07-10 11:32:29 +0200

    added defines of M_PI if they are missed

--- globaldefines.h
+++ globaldefines.h
@@ -1,6 +1,21 @@
 #ifndef GLOBALDEFINES_H
 #define GLOBALDEFINES_H
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+#ifndef M_PI_2
+#define M_PI_2 1.57079632679489661923
+#endif
+#ifndef M_PI_4
+#define M_PI_4 0.785398163397448309616
+#endif
+#ifndef M_1_PI
+#define M_1_PI 0.318309886183790671538
+#endif
+#ifndef M_2_PI
+#define M_2_PI 0.636619772367581343076
+#endif
 
 #define WORLD_SPHERE_RADIUS 10
 #define EARTH_RADIUS 6371.000
